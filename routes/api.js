59cf5ddf7009863abdccc214adeafff5257942e8
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2021-01-22 11:55:44 -0700

    fix cors issues

--- routes/api.js
+++ routes/api.js
@@ -2,23 +2,32 @@ var express = require('express');
 var stateManager = require('../src/stateManager');
 var router = express.Router();
 
+router.use((req, res, next) => {
+    res.set('Access-Control-Allow-Headers', 'content-type');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    next();
+});
+
 router.get('/', function(req, res, next) {
   res.redirect('http://localhost:3000/');
 });
 
 router.post('/get', function(req, res, next) {
+    console.log(req.body)
+
     const data = stateManager.getData(req.body);
-    res.end(data);
+    res.json(data);
 });
 
 router.post('/setValue', function(req, res, next) {
     const data = stateManager.setData(req.body);
-    res.end(data);
+    res.json(data);
 });
 
 router.post('setCurrency', async function(req, res, next) {
     const data = await stateManager.changeCurrency(req.body.currency);
-    res.end(data);
+    res.json(data);
 });
 
 module.exports = router;
